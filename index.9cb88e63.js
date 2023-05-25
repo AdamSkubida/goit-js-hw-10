ec0533fedb97c41a75600989949bc39b7d7b6a1b
AdamSkubida <120182951+AdamSkubida@users.noreply.github.com>
2023-05-25 22:14:32 +0000

    Deploying to gh-pages from @ AdamSkubida/goit-js-hw-10@8c0f101b62e99740c0f32e87a257dd72c66e7ace 🚀

--- index.9cb88e63.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".cat-info");fetch("https://api.thecatapi.com/v1/breeds").then((e=>(console.log(e.status),e.json()))).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));
-//# sourceMappingURL=index.9cb88e63.js.map
