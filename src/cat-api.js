d210a895f7cd86ae5a91d78f01125523093a26b4
AdamSkubida <120182951+AdamSkubida@users.noreply.github.com>
2023-05-25 16:36:59 +0200

    cat-api/update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,5 @@
 export function fetchBreeds() {
-  fetch('https://api.thecatapi.com/v1/breeds', {
-    headers: {
-      Authoryzation: {
-        'x-api-key':
-          'live_iQojlAhxu6F2966aQMXnuLP4Kw1tkswY3n4RuQZ8iHKUo60sctqec2mX4yYoeDCl',
-      },
-    },
-  })
+  fetch('https://api.thecatapi.com/v1/breeds')
     .then(response => {
       console.log(response.status);
       return response.json();
