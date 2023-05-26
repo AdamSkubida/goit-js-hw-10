2dc5280d3b1763aa7291933b92e181e2801073f2
AdamSkubida <120182951+AdamSkubida@users.noreply.github.com>
2023-05-26 19:45:46 +0000

    fetchCatById()/generateImage()/future

--- src/cat-api.js
+++ src/cat-api.js
@@ -15,3 +15,19 @@ export function fetchBreeds() {
     return response.json();
   });
 }
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, {
+      headers: {
+        Authoryzation: {
+          'x-api-key':
+            'live_iQojlAhxu6F2966aQMXnuLP4Kw1tkswY3n4RuQZ8iHKUo60sctqec2mX4yYoeDCl',
+        },
+      },
+    }).then(response => {
+      if(!response.ok){
+        Notify.failure(response.status)
+      }
+      return response.json();
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { fetchBreeds } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const select = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
 
 fetchBreeds()
   .then(cats => renderNewOption(cats))
@@ -16,4 +17,17 @@ function renderNewOption(cats) {
     .join('');
 
   select.innerHTML = template;
+}
+
+
+select.addEventListener('change', (e) => {
+  fetchCatByBreed(e.target.value)
+  .then(cat => generateImage(cat))
+  .catch(error => Notify.failure(error));
+})
+
+function generateImage(cat) {
+  console.log(cat);
+
+
 }
