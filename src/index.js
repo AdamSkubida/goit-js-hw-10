ca1911127008fde75047cb2236a67ec3e3d1dce0
AdamSkubida <adasiow.s@gmail.com>
2023-05-28 16:48:45 +0200

    generate cat with name and temperament

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds, fetchCatByBreed, foo } from './cat-api';
+import { fetchBreeds, fetchCatByBreed, fetchByUniqueId } from './cat-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const select = document.querySelector('.breed-select');
@@ -31,13 +31,43 @@ function generateImage(cat) {
   const catImage = cat
     .map(data => {
       console.log(data);
-      return `<img src="${data.url}" width="500px">`;
+      return `<img class="cat-image" src="${data.url}" width="500px">`;
     })
     .join('');
 
   catInfo.innerHTML = catImage;
+
+  const newBreedId = cat.map(data => {
+    console.log(data.id);
+    return data.id;
+  });
+
+  return fetch(`https://api.thecatapi.com/v1/images/${newBreedId}`)
+    .then(response => {
+      if (!response.ok) {
+        Notify.failure(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+
+      const name = data.breeds[0].name;
+      const description = data.breeds[0].wikipedia_url;
+      const temperament = data.breeds[0].temperament;
+
+      console.log('-----------------------');
+      console.log('INFO:');
+      console.log(name);
+      console.log(description);
+      console.log(temperament);
+      console.log('-----------------------');
+
+      const nameHTML = `<span class="name"><b>${name}</b></span>`;
+      const temperamentHTML = `<span class="temperament"><b>Temperament:</b> ${temperament}</span>`;
+
+      catInfo.insertAdjacentHTML('beforeend', nameHTML);
+      catInfo.insertAdjacentHTML('beforeend', temperamentHTML);
+      return data;
+    });
 }
-
-// {"id":"gCEFbK7in","url":"https://cdn2.thecatapi.com/images/gCEFbK7in.jpg","breeds":[{"weight":{"imperial":"7  -  10","metric":"3 - 5"},"id":"abys","name":"Abyssinian","cfa_url":"http://cfa.org/Breeds/BreedsAB/Abyssinian.aspx","vetstreet_url":"http://www.vetstreet.com/cats/abyssinian","vcahospitals_url":"https://vcahospitals.com/know-your-pet/cat-breeds/abyssinian","temperament":"Active, Energetic, Independent, Intelligent, Gentle","origin":"Egypt","country_codes":"EG","country_code":"EG","description":"The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.","life_span":"14 - 15","indoor":0,"lap":1,"alt_names":"","adaptability":5,"affection_level":5,"child_friendly":3,"dog_friendly":4,"energy_level":5,"grooming":1,"health_issues":2,"intelligence":5,"shedding_level":2,"social_needs":5,"stranger_friendly":5,"vocalisation":1,"experimental":0,"hairless":0,"natural":1,"rare":0,"rex":0,"suppressed_tail":0,"short_legs":0,"wikipedia_url":"https://en.wikipedia.org/wiki/Abyssinian_(cat)","hypoallergenic":0,"reference_image_id":"0XYvRd7oD"}],"width":1424,"height":987}
-
-foo();
